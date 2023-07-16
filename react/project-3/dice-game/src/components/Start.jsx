8fb9fd265792eaf3f90d2bea107d6c34a26af970
Dam <dam17.engineer@gmail.com>
2023-07-17 01:40:52 +0530

    UI with React and Styled Components

---
+++ react/project-3/dice-game/src/components/Start.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { Button } from "../styled/Button";
+
+const Start = ({ toggle }) => {
+  return (
+    <>
+      <Container>
+        <div>
+          <img src="/image/dices-start.png" />
+        </div>
+
+        <div className="content">
+          <h1>DICE GAME</h1>
+          <Button onClick={toggle}>Play Now</Button>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Start;
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  text-align: center;
+
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
+    }
+  }
+`;
